935430c1fa2a3e1ba6f9811478231eb47bc9eb50
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-10-27 12:51:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,23 @@
 #include <sstream>
 using namespace std;
 void selection_sort(int *mas, unsigned int k){  
- for( int i=0; i<k-1; i++){ \\
+ for( int i=0; i<k-1; i++){ 
      int a=i+1;
      int min= mas[i+1];
-     for (int j=i+1; j<k;j++) {
-         if (mas[j]<min) {
+     for (int j=i+1; j<k;j++) 
+     {
+         if (mas[j]<min) 
+         {
              a = j;
              min = mas[j];
          }
      }
      if(min<mas[i]) {
-         swap(mas[i], mas[a]);
-     }
+         swap(mas[i], mas[a])
      }
  }
+ }
+
 int main()
 {
     unsigned int k;                            
@@ -27,9 +30,8 @@ int main()
     istringstream stream (stroka);
     for (unsigned int i=0; i<k;i++){
         stream>> mas[i];
-
-
     }
+ 
     selection_sort (mas,k);
     for(int i=0; i<k; i++){
         cout << mas[i] << " ";
